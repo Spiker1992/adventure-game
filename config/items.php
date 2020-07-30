2fe7b549bee3534b787e75255f4b0243b249768d
Vlad Ogir <vlad@homesearch.co.uk>
2020-07-30 21:23:49 +0100

    Updated a list of item types - made it a lot more indepth

--- config/items.php
+++ config/items.php
@@ -9,13 +9,95 @@ return [
         'name' => 'Durability',
     ]],
     'types' => [[
-        'key' => 'axe',
-        'name' => 'Axe',
+        'key' => 'armors',
+        'name' => 'Armors',
+        'items' => [[
+            'key' => 'head',
+            'name' => 'Head',
+            'items' => [[
+                'key' => 'helms',
+                'name' => 'Helms',
+            ]],
+        ], [
+            'key' => 'shoulders',
+            'name' => 'Shoulders',
+            'items' => [[
+                'key' => 'pauldrons',
+                'name' => 'Pauldrons',
+            ]],
+        ], [
+            'key' => 'torso',
+            'name' => 'Torso',
+            'items' => [[
+                'key' => 'chest_armor',
+                'name' => 'Chest Armor',
+            ]],
+        ], [
+            'key' => 'wrist',
+            'name' => 'Wrist',
+            'items' => [[
+                'key' => 'bracers',
+                'name' => 'Bracers',
+            ]],
+        ], [
+            'key' => 'hands',
+            'name' => 'Hands',
+            'items' => [[
+                'key' => 'gloves',
+                'name' => 'Gloves',
+            ]],
+        ], [
+            'key' => 'waist',
+            'name' => 'Waist',
+            'items' => [[
+                'key' => 'belts',
+                'name' => 'Belts',
+            ]],
+        ], [
+            'key' => 'legs',
+            'name' => 'Legs',
+            'items' => [[
+                'key' => 'pants',
+                'name' => 'Pants',
+            ]],
+        ], [
+            'key' => 'feet',
+            'name' => 'Feet',
+            'items' => [[
+                'key' => 'boots',
+                'name' => 'Boots',
+            ]],
+        ], [
+            'key' => 'off_hand',
+            'name' => 'Off-Hand',
+            'items' => [[
+                'key' => 'shields',
+                'name' => 'Shields',
+            ], [
+                'key' => 'quivers',
+                'name' => 'Quivers',
+            ]],
+        ]],
     ], [
-        'key' => 'sword',
-        'name' => 'Sword',
-    ], [
-        'key' => 'spear',
-        'name' => 'Spear',
+        'key' => 'weapons',
+        'name' => 'Weapons',
+        'items' => [[
+            'key' => 'one_handed',
+            'name' => 'One-Handed',
+            'items' => [[
+                'key' => 'swords',
+                'name' => 'Swords',
+            ], [
+                'key' => 'spears',
+                'name' => 'Spears',
+            ]],
+        ], [
+            'key' => 'ranged',
+            'name' => 'Ranged',
+            'items' => [[
+                'key' => 'bows',
+                'name' => 'Bows',
+            ]],
+        ]],
     ]],
 ];
